d98e8d4538bde72b39b6a49dc81011a76855b275
川上竜太郎 <ryutaro@kawakamiryuutarounoMacBook-Pro.local>
2019-05-08 23:12:59 +0900

    記事詳細ページ追加

---
+++ pages/articles/_id.vue
@@ -0,0 +1,18 @@
+<template>
+  <section>
+    articleDetailPage
+  </section>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "nuxt-property-decorator";
+import { State } from "vuex-class";
+
+@Component({
+  components: {}
+})
+export default class ArticleDetail extends Vue {}
+</script>
+
+<style scoped>
+</style>
